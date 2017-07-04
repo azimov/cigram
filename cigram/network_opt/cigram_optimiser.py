c0c41861ba28b708ccdffa568727c83cd323335e
James Gilbert <james.gilbert@nottingham.ac.uk>
2017-07-04 11:54:36 +0100

    More pep8 compliance and code cleanup

--- cigram/network_opt/cigram_optimiser.py
+++ cigram/network_opt/cigram_optimiser.py
@@ -12,38 +12,28 @@ from collections import OrderedDict
 import logging
 logger = logging.getLogger('inspyred.ec')
 
+
 class CigramOptimiser(object):
-    """
-    Optimisation class to fit a given type of graph model.
-    This code could be generalised to another type of model to adapt the fitting procedure
-    """
-    model_params = []
     
     def __init__(self, g, fit_evaluator,
-                    a_bounds=(-5.0, 5.0),
-                    sf_bounds=(0.6, 2.0),
-                    sr_bounds=(0.6, 2.0),
-                    csf_bounds=(0.8, 2.0),
-                    csr_bounds=(0.8, 2.0),
-                    ek_per_bounds=(0.01, 0.4),
-                    po_bounds=(0.01, 0.15),
-                    max_n=None,
-                    k_bounds=None,
-                    random=np.random,
-                    min_degree=1,
-                    starting_population=[],
-                    graph_props=None,
-                    seed=None,
-                    **kwargs):
+                 a_bounds=(-5.0, 5.0),
+                 sf_bounds=(0.6, 2.0),
+                 sr_bounds=(0.6, 2.0),
+                 csf_bounds=(0.8, 2.0),
+                 csr_bounds=(0.8, 2.0),
+                 ek_per_bounds=(0.01, 0.4),
+                 po_bounds=(0.01, 0.15),
+                 max_n=None,
+                 k_bounds=None,
+                 random=np.random,
+                 min_degree=1,
+                 starting_population=None,
+                 graph_props=None,
+                 seed=None,
+                 **kwargs):
         """
-        Parameters that are used throughout the optimisation are set here.
-        fixed_model_params are the parameters that do no change throughout
-        param boundaries are the limits of the parameters that can be used.
-        The weights apply as follows:
-                max_degree_fit_weight - the difference between the (log scaled) maximum degrees.
-                degree_fit_weight - the distance between observed degree distrubtions in the Kolmogorov Smirnov distance.
-                assort_fit_weight - the distance between observed levels of assortativity.
-
+        Optimisation class to fit a given type of graph model.
+        This code could be generalised to another type of model to adapt the fitting procedure
 
 
         :param g: networkx.Graph object to fit
@@ -69,12 +59,16 @@ class CigramOptimiser(object):
         self.fixed_model_params = {
                 "n": g.number_of_nodes(),
                 "density": nx.density(g),
-                "connected":False,
-                "min_degree":min_degree
+                "connected": False,
+                "min_degree": min_degree
         }
         
         self.gbest = None
-        self.starting_population =  starting_population
+
+        if starting_population is None:
+            starting_population = []
+
+        self.starting_population = starting_population
         
         if max_n is not None and max_n < g.number_of_nodes():
                 scaling_factor = g.number_of_nodes()/max_n
@@ -86,17 +80,17 @@ class CigramOptimiser(object):
         
         # Fix other parameters if we're not generating community strucuture
         if k_bounds == 1:
-                csf_bounds=0
-                csr_bounds=0
-                ek_per_bounds=0
-                po_bounds=0
+                csf_bounds = 0
+                csr_bounds = 0
+                ek_per_bounds = 0
+                po_bounds = 0
 
         # Set the boundaries of the optimisation that map to the parameters.
         self.param_boundaries = OrderedDict()
         self.param_boundaries["sigma_nodes"] = sf_bounds
         self.param_boundaries["sigma_edges"] = sr_bounds
         self.param_boundaries["a"] = a_bounds
-        self.param_boundaries["k"] =  k_bounds
+        self.param_boundaries["k"] = k_bounds
         self.param_boundaries["community_sigma_f"] = csf_bounds
         self.param_boundaries["community_sigma_r"] = csr_bounds
         self.param_boundaries["ek_per"] = ek_per_bounds
@@ -124,11 +118,16 @@ class CigramOptimiser(object):
         if graph_props is not None:
                 self.graph_props_a = graph_props
         else: 
-                self.graph_props_a =  self.fit_evaluator.graph_properties(G)
+                self.graph_props_a = self.fit_evaluator.graph_properties(g)
                 
-        self.seed=seed
+        self.seed = seed
     
-    def set_gbest(self, pbest):	
+    def set_gbest(self, pbest):
+        """
+        Updates the best solution
+        :param pbest: solution
+        :return: None
+        """
         if self.gbest is None or self.gbest[1] > pbest[1]:
                 self.gbest = pbest
     
@@ -155,11 +154,11 @@ class CigramOptimiser(object):
         logger.debug("Generated {0}".format(params))
         return g
 
-
-    def parameter_generator(self, random, args):
+    def parameter_generator(self):
         """
-        Geneartes a random set of parameters for use by the optimiser
-        if the starting population kwarg is used at init, the population is not randomised but starts with specified values
+        Generates a random set of parameters for use by the optimiser
+        if the starting population kwarg is used at init, the population is not randomised but starts with specified
+        values
         """
         
         if len(self.starting_population):
@@ -169,7 +168,6 @@ class CigramOptimiser(object):
             pset = [self.random_param_func[p](*bounds) for p, bounds in self.param_boundaries.items()]
         return pset
 
-
     def convert_params(self, candidate):
         """
         A candidate set will be in a given order (specified by the parameter_generator method)
@@ -178,44 +176,41 @@ class CigramOptimiser(object):
         keys = self.param_boundaries.keys()
         return dict((keys[i], candidate[i]) for i in range(len(candidate)))
 
-
     def get_candidate(self, parameters):
         """
         Given params, convert to a given candidate format
         """
         return [parameters[p] for p in self.param_boundaries]
 
-
     def get_params(self, candidate):
         
         return dict(self.fixed_model_params.items() + self.convert_params(candidate).items())
 
-
     def get_fitness(self, candidate):
         """
         given a set of parameters, evaluate the fitness
-        This function tests the assortativity and distance of the degree distribution (using the KS and log difference of max degree)
+        This function tests the assortativity and distance of the degree distribution (using the KS and log difference
+        of max degree)
         Create a subclass of this class to change this function.
         """
-        M = self.generate(candidate)
-        graph_props_b =  self.fit_evaluator.graph_properties(M)
-        del M
+        m = self.generate(candidate)
+        graph_props_b = self.fit_evaluator.graph_properties(m)
+        del m
         return self.fit_evaluator.graph_fitness(self.graph_props_a, graph_props_b)
-    
     
     def get_fitness_avg(self, candidate, num_reps=1):
         """
         given a set of parameters, evaluate the fitness over average range of samples
-        This function tests the assortativity and distance of the degree distribution (using the KS and log difference of max degree)
+        This function tests the assortativity and distance of the degree distribution (using the KS and log difference
+        of max degree)
         Create a subclass of this class to change this function.
         """
         
         if num_reps < 1:
                 num_reps = 1
-        graphs = [self.generate(candidate) for s in range(num_reps)]
+        graphs = [self.generate(candidate) for _ in range(num_reps)]
         graph_props_b = self.fit_evaluator.graph_properties_avg(graphs)
         return self.fit_evaluator.graph_fitness(self.graph_props_a, graph_props_b)
-    
 
     def bounder(self):
         """
@@ -225,5 +220,3 @@ class CigramOptimiser(object):
         upper_constraints = tuple([v[1] for v in self.param_boundaries.values()])
         
         return inspyred.ec.Bounder(lower_constraints, upper_constraints)
-        
-        
