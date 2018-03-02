8e1b489b9970bb44b7352588c23831dfa3c67951
james gilbert <james.gilbert@nottingham.ac.uk>
2018-03-02 13:31:03 +0000

    Changes for required packages

--- setup.py
+++ setup.py
@@ -19,12 +19,25 @@ lfr_sources = [
 cmodule = Extension("cigram.cmodel", sources=sources, extra_compile_args=["-Ofast"])
 lfrmodule = Extension("cigram.lfr_model", sources=lfr_sources, extra_compile_args=["-Wno-undef", "-Ofast"])
 
+long_description='''
+CiGRAM is a generator for random complex networks with community structure and assortative connections.
+CiGRAM can be used as a benchmark for community detection algorithms.
+
+This package also includes LFR benchmark models.
+'''
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
 setup(
     name="cigram",
     version="0.1.0",
     description="Circular Gaussian Random gRAph Model - a generator for synthetic complex networks",
+    long_description=long_description,
     zip_safe=False,
     author="James Gilbert",
+    install_requires=requirements,
     author_email="jamie.gilbert@azimov.co.uk",
     license="GPL",
     entry_points={
@@ -35,4 +48,8 @@ setup(
     include_package_data=True,
     packages=["cigram", "cigram.network_opt"],
     download_url="https://github.com/azimov/cigram/archive/0.1.0.tar.gz",
+    project_urls=dict(
+        documentation='https://cigram.readthedocs.org',
+        visualisation='http://cigram.ico2s.org',
+    ),
 )
